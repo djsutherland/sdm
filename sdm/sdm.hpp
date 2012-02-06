547b5e364f76ea417b3e8350871d7cd08bb9aafe
Dougal Sutherland <dougal@gmail.com>
2012-02-06 03:30:03 -0500

    not-yet-working basic implementation of training

--- sdm/sdm.hpp
+++ sdm/sdm.hpp
@@ -32,51 +32,123 @@
 #define SDM_HPP_
 #include "sdm/basics.hpp"
 #include "sdm/kernels.hpp"
+#include "sdm/kernel_projection.hpp"
 
+#include <algorithm>
+#include <stdexcept>
 #include <vector>
 
-#include <np-divs/div_params.hpp>
+#include <np-divs/matrix_arrays.hpp>
+#include <np-divs/np_divs.hpp>
 #include <flann/util/matrix.h>
 #include <svm.h>
 
 namespace sdm {
 
+// TODO memory ownership with this is all screwy...make it clearer
 template <typename Scalar>
 class SDM {
-    svm_model &svm;
-    Kernel &kernel;
+    const svm_model &svm;
+    const svm_problem &svm_prob; // needs to live at least as long as the model
+    const Kernel &kernel;
 
     const flann::Matrix<Scalar> *training_bags;
     size_t num_training;
 
     public:
-        SDM(svm_model &svm, Kernel &kernel,
+        SDM(const svm_model &svm, const svm_problem &svm_prob,
+            const Kernel &kernel,
             const flann::Matrix<Scalar> *training_bags, size_t num_training)
         :
-            svm(svm), kernel(kernel),
+            svm(svm), svm_prob(svm_prob), kernel(kernel),
             training_bags(training_bags), num_training(num_training)
         { }
 
         std::vector<bool> predict(
-                const flann::Matrix<Scalar> *test_dists, size_t num_test);
+                const flann::Matrix<Scalar> *test_dists, size_t num_test)
+            const;
 
         std::vector<bool> predict(
                 const flann::Matrix<Scalar> *test_dists, size_t num_test,
-                std::vector<double> &values);
+                std::vector<double> &values)
+            const;
 
-
-        // TODO: a mass-training method for more than one Kernel
-        static SDM train(
-                const flann::Matrix<Scalar> *training_bags, size_t num_train,
-                const std::vector<bool> &labels,
-                const Kernel &kernel,
-                const NPDivs::DivParams &div_params,
-                const svm_parameter &svm_params
-                // cross-validation options
-        );
 
 };
 
+// TODO: support multi-class classification
+// TODO: a mass-training method for more than one kernel
+template <typename Scalar>
+SDM<Scalar> train_sdm(
+    const flann::Matrix<Scalar> *training_bags, size_t num_train,
+    const std::vector<int> &labels,
+    const Kernel &kernel,
+    const NPDivs::DivParams &div_params,
+    const svm_parameter &svm_params
+    // TODO: cross-validation options
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// Template member function implementations
+
+void store_kernel_matrix(svm_problem &prob, double *divs, bool alloc) {
+    size_t n = prob.l;
+    if (alloc) prob.x = new svm_node*[n];
+
+    for (size_t i = 0; i < n; i++) {
+        if (alloc) prob.x[i] = new svm_node[n];
+        for (size_t j = 0; j < n; j++) {
+            prob.x[i][j].index = j;
+            prob.x[i][j].value = divs[i + j*n];
+        }
+    }
 }
+
+template <typename Scalar>
+SDM<Scalar> train_sdm(
+        const flann::Matrix<Scalar> *training_bags, size_t num_train,
+        const std::vector<int> &labels,
+        const Kernel &kernel,
+        const NPDivs::DivParams &div_params,
+        const svm_parameter &svm_params)
+{   // TODO - logging
+
+    // copy the svm params so we can change them
+    svm_parameter svm_p = svm_params;
+    svm_p.svm_type = C_SVC;
+    svm_p.kernel_type = PRECOMPUTED;
+
+    // set up the basic svm_problem, except for the kernel values
+    svm_problem *prob = new svm_problem;
+    prob->l = num_train;
+    prob->y = new double[num_train];
+    std::copy(labels.begin(), labels.end(), prob->y);
+    // for (size_t i = 0; i < num_train; i++)
+    //     prob->y[i] = labels[i];
+
+    // first compute divergences
+    flann::Matrix<double>* divs =
+        NPDivs::alloc_matrix_array<double>(1, num_train, num_train);
+    np_divs(training_bags, num_train, kernel.getDivFunc(), divs,
+            div_params, false);
+
+    // TODO cross-validate over possibilities for the svm/kernel parameters
+
+    // build up our best kernel matrix
+    kernel.transformDivergences(divs->ptr(), num_train);
+    project_to_kernel(divs->ptr(), num_train);
+
+    // train an SVM on the whole thing
+    // TODO after doing CV, won't need to alloc here
+    store_kernel_matrix(*prob, divs->ptr(), true);
+    const char* error = svm_check_parameter(prob, &svm_p);
+    if (error != NULL)
+        throw std::domain_error(error);
+    svm_model *svm = svm_train(prob, &svm_p);
+
+    return SDM<Scalar>(*svm, *prob, kernel, training_bags, num_train);
+}
+
+} // end namespace
 
 #endif
--- test/tests.cpp
+++ test/tests.cpp
@@ -5,7 +5,14 @@
 #include <iostream>
 #include <stdexcept>
 
+#include <np-divs/div-funcs/div_l2.hpp>
+#include <np-divs/div_params.hpp>
+
 #include "sdm/kernel_projection.hpp"
+#include "sdm/kernels.hpp"
+#include "sdm/sdm.hpp"
+
+#include <svm.h>
 
 namespace {
 
@@ -52,6 +59,68 @@ TEST(UtilitiesTest, KernelProjection) {
 
     for (size_t i = 0; i < 25; i++)
         EXPECT_NEAR(mat[i], kernel[i], 1e-4) << "i = " << i;
+}
+
+TEST(SDMTest, BasicTraining) {
+    typedef flann::Matrix<double> Matrix;
+
+    // set up a stupid dataset: 1D N(0,1) vs N(.5, .5) with size-ten samples
+    double train11[] = {0.5377, 1.8339, -2.2588, 0.8622, 0.3188, -1.3077,
+        -0.4336, 0.3426, 3.5784, 2.7694};
+    double train12[] = {0.8884, -1.1471, -1.0689, -0.8095, -2.9443, 1.4384,
+        0.3252, -0.7549, 1.3703, -1.7115};
+    double train13[] = {-0.1022, -0.2414, 0.3192, 0.3129, -0.8649, -0.0301,
+        -0.1649, 0.6277, 1.0933, 1.1093};
+    double train14[] = {-0.8637, 0.0774, -1.2141, -1.1135, -0.0068, 1.5326,
+        -0.7697, 0.3714, -0.2256, 1.1174};
+    double train15[] = {-1.0891, 0.0326, 0.5525, 1.1006, 1.5442, 0.0859,
+        -1.4916, -0.7423, -1.0616, 2.3505};
+
+    double train21[] = {1.2097, 0.6458, 0.5989, 1.2938, 0.0978, 0.8483,
+        0.9175, 0.3781, 0.6078, -0.0829};
+    double train22[] = {-0.0740, 0.5524, 0.8611, 1.7927, 0.1666, 0.5937,
+        0.4588, -0.4665, 0.2805, -0.3973};
+    double train23[] = {0.9202, 0.0560, 0.5500, 0.2277, 0.6518, 0.1998,
+        0.7450, 0.8697, 1.3559, 0.4029};
+    double train24[] = {-0.5692, 0.0802, 1.1773, -0.0361, 0.9805, 0.5620,
+        1.2183, -0.4804, 0.4012, -0.1039};
+    double train25[] = {1.9540, 0.9126, 1.1895, -0.0291, 0.2657, 0.3638,
+        1.0492, 0.3611, 0.8508, -0.5259};
+
+    Matrix train[5];
+    train[0] = Matrix(train11, 10, 1);
+    train[1] = Matrix(train12, 10, 1);
+    train[2] = Matrix(train13, 10, 1);
+    train[3] = Matrix(train14, 10, 1);
+    train[4] = Matrix(train15, 10, 1);
+    train[5] = Matrix(train21, 10, 1);
+    train[6] = Matrix(train22, 10, 1);
+    train[7] = Matrix(train23, 10, 1);
+    train[8] = Matrix(train24, 10, 1);
+    train[9] = Matrix(train25, 10, 1);
+
+    std::vector<int> labels(10);
+    for (size_t i = 0; i < 5; i++) {
+        labels[i] = 0;
+        labels[i+5] = 1;
+    }
+
+    NPDivs::DivL2 div_func;
+    LinearKernel kernel(div_func);
+
+    NPDivs::DivParams div_params;
+    div_params.num_threads = 1;
+
+    svm_parameter svm_params;
+    svm_params.cache_size = 10;
+    svm_params.C = 2;
+    svm_params.eps = 1e-3;
+    svm_params.nr_weight = 0;
+    svm_params.shrinking = 1;
+    svm_params.probability = 1;
+
+    const SDM<double> &model =
+        train_sdm(train, 10, labels, kernel, div_params, svm_params);
 }
 
 } // end namespace
