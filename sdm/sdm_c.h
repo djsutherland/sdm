a6a6625cbb540d23b2553af9c66385b94180472b
Dougal Sutherland <dougal@gmail.com>
2012-07-05 18:04:31 -0400

    fix tabs, etc in svm_parameter def'n

--- sdm/sdm_c.h
+++ sdm/sdm_c.h
@@ -56,21 +56,21 @@ extern "C" {
 struct svm_parameter {
     int svm_type;
     int kernel_type;
-    int degree;	/* for poly */
-    double gamma;	/* for poly/rbf/sigmoid */
-    double coef0;	/* for poly/sigmoid */
+    int degree;   /* for poly */
+    double gamma; /* for poly/rbf/sigmoid */
+    double coef0; /* for poly/sigmoid */
     
     /* these are for training only */
     double cache_size; /* in MB */
-    double eps;	/* stopping criteria */
-    double C;	/* for C_SVC, EPSILON_SVR and NU_SVR */
-    int nr_weight;		/* for C_SVC */
-    int *weight_label;	/* for C_SVC */
-    double* weight;		/* for C_SVC */
-    double nu;	/* for NU_SVC, ONE_CLASS, and NU_SVR */
-    double p;	/* for EPSILON_SVR */
-    int shrinking;	/* use the shrinking heuristics */
-    int probability; /* do probability estimates */
+    double eps;        /* stopping criteria */
+    double C;          /* for C_SVC, EPSILON_SVR and NU_SVR */
+    int nr_weight;     /* for C_SVC */
+    int *weight_label; /* for C_SVC */
+    double* weight;    /* for C_SVC */
+    double nu;         /* for NU_SVC, ONE_CLASS, and NU_SVR */
+    double p;          /* for EPSILON_SVR */
+    int shrinking;     /* use the shrinking heuristics */
+    int probability;   /* do probability estimates */
 };
 
 enum { C_SVC, NU_SVC, ONE_CLASS, EPSILON_SVR, NU_SVR };
